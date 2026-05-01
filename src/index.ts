96b2cf82470d05f40930be2ed82f1c2c257015bf
Corgol <31749897+corgol@users.noreply.github.com>
2026-05-02 01:10:35 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,107 +1,167 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Demo of CORS Anywhere</title>
-<meta name="viewport" content="width=device-width">
-<style>
-html, body {
-  margin: 0;
-  height: 100%;
-  padding: 3px;
-  font-family: Arial, sans-serif;
-  font-size: 16px;
-}
-* {
-  -moz-box-sizing: border-box;
-       box-sizing: border-box;
-}
-label { display: block; }
-input {
-  display: block;
-  width: 100%;
-  padding: 8px 5px;
-  border: 1px solid #CCC;
-}
-button {
-  display: inline-block;
-  width: 49%;
-  padding: 8px;
-}
-textarea {
-  width: 100%;
-  height: 100%;
-}
-#top {
-  height: 180px;
-  position: relative;
+/*
+CORS Anywhere as a Cloudflare Worker!
+(c) 2019 by Zibri (www.zibri.org)
+email: zibri AT zibri DOT org
+https://github.com/Zibri/cloudflare-cors-anywhere
 
-}
-#bottom {
-  height: 100%;
-  margin-top: -180px;
-  padding-top: 180px;
-}
-</style>
-</head>
-<body>
-<div id="top">
-  CORS Anywhere demo &bull; <a href="https://github.com/Rob--W/cors-anywhere/">Github</a> &bull; <a href="http://cors-anywhere.herokuapp.com">Live server</a>.
-  <label>
-    Url to be fetched (example: <a href="//robwu.nl/dump.php">robwu.nl/dump.php</a>)
-    <input type="url" id="url" value="">
-  </label>
-  <label>
-    If using POST, enter the data:
-    <input type="text" id="data">
-  </label>
-  <label>
-    <button id="get">GET</button><button id="post">POST</button>
-  </label>
-</div>
-<div id="bottom">
-  <textarea id="output"></textarea>
-</div>
+This Cloudflare Worker script acts as a CORS proxy that allows
+cross-origin resource sharing for specified origins and URLs.
+It handles OPTIONS preflight requests and modifies response headers accordingly to enable CORS.
+The script also includes functionality to parse custom headers and provide detailed information
+about the CORS proxy service when accessed without specific parameters.
+The script is configurable with whitelist and blacklist patterns, although the blacklist feature is currently unused.
+The main goal is to facilitate cross-origin requests while enforcing specific security and rate-limiting policies.
+*/
 
-<script>
-  var cors_api_url = 'https://cors-proxy.cooks.fyi/';
-  function doCORSRequest(options, printResult) {
-    var x = new XMLHttpRequest();
-    x.open(options.method, cors_api_url + options.url);
-    x.onload = x.onerror = function() {
-      printResult(
-        options.method + ' ' + options.url + '\n' +
-        x.status + ' ' + x.statusText + '\n\n' +
-        (x.responseText || '')
-      );
-    };
-    if (/^POST/i.test(options.method)) {
-      x.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+// Configuration: Whitelist and Blacklist (not used in this version)
+// whitelist = [ "^http.?://www.zibri.org$", "zibri.org$", "test\\..*" ];  // regexp for whitelisted urls
+const blacklistUrls = [];           // regexp for blacklisted urls
+const whitelistOrigins = [ ".*" ];   // regexp for whitelisted origins
+
+// Function to check if a given URI or origin is listed in the whitelist or blacklist
+function isListedInWhitelist(uri, listing) {
+    let isListed = false;
+    if (typeof uri === "string") {
+        listing.forEach((pattern) => {
+            if (uri.match(pattern) !== null) {
+                isListed = true;
+            }
+        });
+    } else {
+        // When URI is null (e.g., when Origin header is missing), decide based on the implementation
+        isListed = true; // true accepts null origins, false would reject them
     }
-    x.send(options.data);
-  }
+    return isListed;
+}
 
-  // Bind event
-  (function() {
-    var urlField = document.getElementById('url');
-    var dataField = document.getElementById('data');
-    var outputField = document.getElementById('output');
-    document.getElementById('get').onclick =
-    document.getElementById('post').onclick = function(e) {
-      e.preventDefault();
-      doCORSRequest({
-        method: this.id === 'post' ? 'POST' : 'GET',
-        url: urlField.value,
-        data: dataField.value
-      }, function printResult(result) {
-        outputField.value = result;
-      });
-    };
-  })();
-  if (typeof console === 'object') {
-    console.log('// To test a local CORS Anywhere server, set cors_api_url. For example:');
-    console.log('cors_api_url = "https://corgol.com/"');
-  }
-</script>
-</body>
-</html>
+// Event listener for incoming fetch requests
+addEventListener("fetch", async event => {
+    event.respondWith((async function() {
+        const isPreflightRequest = (event.request.method === "OPTIONS");
+        
+        const originUrl = new URL(event.request.url);
+
+        // Function to modify headers to enable CORS
+        function setupCORSHeaders(headers) {
+            headers.set("Access-Control-Allow-Origin", event.request.headers.get("Origin"));
+            if (isPreflightRequest) {
+                headers.set("Access-Control-Allow-Methods", event.request.headers.get("access-control-request-method"));
+                const requestedHeaders = event.request.headers.get("access-control-request-headers");
+
+                if (requestedHeaders) {
+                    headers.set("Access-Control-Allow-Headers", requestedHeaders);
+                }
+
+                headers.delete("X-Content-Type-Options"); // Remove X-Content-Type-Options header
+            }
+            return headers;
+        }
+
+        const targetUrl = decodeURIComponent(decodeURIComponent(originUrl.search.substr(1)));
+
+        const originHeader = event.request.headers.get("Origin");
+        const connectingIp = event.request.headers.get("CF-Connecting-IP");
+
+        if ((!isListedInWhitelist(targetUrl, blacklistUrls)) && (isListedInWhitelist(originHeader, whitelistOrigins))) {
+            let customHeaders = event.request.headers.get("x-cors-headers");
+
+            if (customHeaders !== null) {
+                try {
+                    customHeaders = JSON.parse(customHeaders);
+                } catch (e) {}
+            }
+
+            if (originUrl.search.startsWith("?")) {
+                const filteredHeaders = {};
+                for (const [key, value] of event.request.headers.entries()) {
+                    if (
+                        (key.match("^origin") === null) &&
+                        (key.match("eferer") === null) &&
+                        (key.match("^cf-") === null) &&
+                        (key.match("^x-forw") === null) &&
+                        (key.match("^x-cors-headers") === null)
+                    ) {
+                        filteredHeaders[key] = value;
+                    }
+                }
+
+                if (customHeaders !== null) {
+                    Object.entries(customHeaders).forEach((entry) => (filteredHeaders[entry[0]] = entry[1]));
+                }
+
+                const newRequest = new Request(event.request, {
+                    redirect: "follow",
+                    headers: filteredHeaders
+                });
+
+                const response = await fetch(targetUrl, newRequest);
+                const responseHeaders = new Headers(response.headers);
+                const exposedHeaders = [];
+                const allResponseHeaders = {};
+                for (const [key, value] of response.headers.entries()) {
+                    exposedHeaders.push(key);
+                    allResponseHeaders[key] = value;
+                }
+                exposedHeaders.push("cors-received-headers");
+                responseHeaders = setupCORSHeaders(responseHeaders);
+
+                responseHeaders.set("Access-Control-Expose-Headers", exposedHeaders.join(","));
+                responseHeaders.set("cors-received-headers", JSON.stringify(allResponseHeaders));
+
+                const responseBody = isPreflightRequest ? null : await response.arrayBuffer();
+
+                const responseInit = {
+                    headers: responseHeaders,
+                    status: isPreflightRequest ? 200 : response.status,
+                    statusText: isPreflightRequest ? "OK" : response.statusText
+                };
+                return new Response(responseBody, responseInit);
+
+            } else {
+                const responseHeaders = new Headers();
+                responseHeaders = setupCORSHeaders(responseHeaders);
+
+                let country = false;
+                let colo = false;
+                if (typeof event.request.cf !== "undefined") {
+                    country = event.request.cf.country || false;
+                    colo = event.request.cf.colo || false;
+                }
+
+                return new Response(
+                    "CLOUDFLARE-CORS-ANYWHERE\n\n" +
+                    "Source:\nhttps://github.com/Zibri/cloudflare-cors-anywhere\n\n" +
+                    "Usage:\n" +
+                    originUrl.origin + "/?uri\n\n" +
+                    "Donate:\nhttps://paypal.me/Zibri/5\n\n" +
+                    "Limits: 100,000 requests/day\n" +
+                    "          1,000 requests/10 minutes\n\n" +
+                    (originHeader !== null ? "Origin: " + originHeader + "\n" : "") +
+                    "IP: " + connectingIp + "\n" +
+                    (country ? "Country: " + country + "\n" : "") +
+                    (colo ? "Datacenter: " + colo + "\n" : "") +
+                    "\n" +
+                    (customHeaders !== null ? "\nx-cors-headers: " + JSON.stringify(customHeaders) : ""),
+                    {
+                        status: 200,
+                        headers: responseHeaders
+                    }
+                );
+            }
+        } else {
+            return new Response(
+                "Create your own CORS proxy</br>\n" +
+                "<a href='https://github.com/Zibri/cloudflare-cors-anywhere'>https://github.com/Zibri/cloudflare-cors-anywhere</a></br>\n" +
+                "\nDonate</br>\n" +
+                "<a href='https://paypal.me/Zibri/5'>https://paypal.me/Zibri/5</a>\n",
+                {
+                    status: 403,
+                    statusText: 'Forbidden',
+                    headers: {
+                        "Content-Type": "text/html"
+                    }
+                }
+            );
+        }
+    })());
+});
